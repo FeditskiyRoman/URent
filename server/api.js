8409bd7be5fd475ced2ac42202ce50d3911f58b1
Roman Feditskiy <feditskiy@gmail.com>
2018-08-02 12:18:05 +0300

    Added main server and frontend files

---
+++ server/api.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const Models = require('./models/Models');
+const crypto = require('crypto');
+const jwt = require('express-jwt/lib');
+const auth = jwt({
+  secret: 'MY_SECRET',
+  userProperty: 'payload'
+});
+
+const ctrlProfile = require('./controllers/profile');
+const ctrlAuth = require('./controllers/authentication');
+
+const router = express.Router(); /** Initializing Routes instance **/
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
+// profile info
+router.get('/api/profile', auth, ctrlProfile.profileRead);
+
+// profile update
+router.put('/api/update', auth, ctrlProfile.profileUpdate);
+
+// profile update Password
+router.put('/api/password', auth, ctrlProfile.updatePassword);
+
+// profile deleteUser
+router.delete('/api/delete', auth, ctrlProfile.deleteUser);
+
+// login-register authentication
+router.post('/api/register', ctrlAuth.register);
+router.post('/api/login', ctrlAuth.login);
+
+module.exports = router;
